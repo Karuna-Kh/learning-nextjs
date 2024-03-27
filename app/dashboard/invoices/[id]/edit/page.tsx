8ac5bb71ae4137fa8fb182d348ec3dcb486fd4f4
karuna-kh <khunneng007@gmail.com>
2024-03-27 17:53:01 +0700

    #

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Edit Invoice | Acme Dashboard',
+  title: 'Edit',
 };
 
 export default async function Page({ params }: { params: { id: string } }) {
